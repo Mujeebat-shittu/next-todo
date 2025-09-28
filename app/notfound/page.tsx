fe8f2e083cee3a4c8ca0139b0883c153fcd93bb5
Mujeebat Shittu <shittuhoney@gmail.com>
2025-09-28 01:30:09 +0100

    fixing lint issues

--- app/notfound/page.tsx
+++ app/notfound/page.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
+
+
 export default function NotFound() {
   return (
     <div className="flex items-center justify-center h-screen text-center">
       <div>
         <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
-        <p className="mt-4 text-gray-400">Oops! The page you're looking for doesn't exist.</p>
-        <a href="/" className="text-blue-500 hover:underline mt-4 inline-block">
+        <p className="mt-4 text-gray-400">Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
+        <Link href="/" className="text-blue-500 hover:underline mt-4 inline-block">
           Go back home
-        </a>
+        </Link>
       </div>
     </div>
   );
